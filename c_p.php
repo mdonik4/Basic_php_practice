20620943e1c6f485b74508e9dfaf6c2a7237d56b
mdonik4 <mdonik4@gmail.com>
2024-12-28 12:25:55 +0600

    Problem_solving

---
+++ c_p.php
@@ -0,0 +1,17 @@
+<?php
+
+$a = 70;
+$b = 20;
+
+$sum = $a + $b ;
+$subtraction = $a - $b;
+$multiplication = $a * $b;
+$division = $a / $b;
+
+echo "Sum-Result : $sum<br> ";
+echo "subtraction-Result : $subtraction<br> ";
+echo "multiplication-Result : $multiplication<br> ";
+echo "division-Result : $division<br> ";
+
+
+?>
---
+++ challenge_2.php
@@ -0,0 +1,21 @@
+<?php
+/*
+
+$celsius = 15;
+$fahrenheit = ($celsius * 9/5) + 32;
+
+echo "Celsius : " . $celsius . "°C" . "<br>";
+echo "Fahrenheit : " . $fahrenheit . "°F" . "<br>";
+
+*/
+
+$celsius = 10;
+
+$fahrenheit = ($celsius *9/5) +32;
+
+echo "Celsius : $celsius " . "°C" . "<br>";
+echo "Fahrenheit : $fahrenheit " . "°F" . "<br>";
+  
+
+
+?>
